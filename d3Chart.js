9b9c6e3334dfaefded875e1888303502d395c56b
Germain Boue <boue.germain@gmail.com>
2015-10-07 20:38:09 -0700

    HTML skeleton linking D3 Chart JS file and D3 library

--- d3Chart.js
+++ d3Chart.js
@@ -1,1 +1,13 @@
 var d3Chart = {};
+
+d3Chart.create = function(el, props, state) {
+  var svg = d3.select(el).append('svg')
+      .attr('class', 'd3')
+      .attr('width', props.width)
+      .attr('height', props.height);
+
+  svg.append('g')
+      .attr('class', 'd3-points');
+
+  this.update(el, state);
+};
