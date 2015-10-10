8d4cb631edfcc521c31651d35cf9cc96aa9d48ae
Germain Boue <boue.germain@gmail.com>
2015-10-10 13:01:46 -0700

    Edit chart height weight

--- Chart.js
+++ Chart.js
@@ -5,4 +5,12 @@ var Chart = React.createClass({
     data: React.PropTypes.array,
     domain: React.PropTypes.object
   },
+
+  componentDidMount: function(){
+    var el = this.getDOMNode();
+    d3Chart.create(el, {
+      width: '100%',
+      height: '300px'
+    }, this.getChartState());
+  },
 });
